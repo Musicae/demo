fe2a830c4fcb4721b65a8eeaeabf8f2ec6c55e84
Aymeric Auberger <aymeric.auberger@gmail.com>
2016-07-31 19:01:34 +0200

    navbar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Musicae' });
 });
 
+/* GET interpretation page. */
+router.get('/goldberg-variations-bmw-998/glenn-gloud-1981', function(req, res, next) {
+  res.render('demo-gould-1981', { title: 'Musicae - J.S. Bach - Goldberg Variations, BMW 998 - Glenn Gould, 1981' });
+});
+
 module.exports = router;
