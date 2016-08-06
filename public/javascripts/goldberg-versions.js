84e2ad0b06c626363b6e5b8962c6c565e9b434ce
Pierre Pariente <pierrepariente@Pierres-MacBook-Pro-2.local>
2016-08-07 01:21:02 +0200

    Access sidebar

--- public/javascripts/goldberg-versions.js
+++ public/javascripts/goldberg-versions.js
@@ -8,6 +8,17 @@ $(document).ready(function() {
     $(this).children().first().animate({'top': '300'}, 400);
   });
 
+  (function() {
+    var bodyEl = $('body'),
+        navToggleBtn = bodyEl.find('.nav-toggle-btn');
+
+    navToggleBtn.on('click', function(e) {
+      bodyEl.toggleClass('active-nav');
+      e.preventDefault();
+    });
+
+  })();
+
 
 
 
