c0e587e747894b50ad467e0cca992c89ab927f2f
Aymeric Auberger <aymeric.auberger@gmail.com>
2016-08-09 17:32:30 +0200

    Change the sidebar, menu changes, doing data spy

--- public/javascripts/goldberg-versions.js
+++ public/javascripts/goldberg-versions.js
@@ -8,24 +8,38 @@ $(document).ready(function() {
     $(this).children().first().animate({'top': '300'}, 400);
   });
 
-  (function() {
-    var bodyEl = $('body'),
-        navToggleBtn = bodyEl.find('.nav-toggle-btn');
-
-    navToggleBtn.on('click', function(e) {
-      bodyEl.toggleClass('active-nav');
-      e.preventDefault();
-    });
-
-  })();
-
-  $('.nav-toggle-btn').click(function() {
-    $(".box").toggleClass('col-sm-10');
+  $('#nav-open').click(function(e) {
+    e.preventDefault();
+    $('#sidebar').show();
+    $("#page-content").toggleClass('col-sm-10', true);
+    $(".content-box").children().toggleClass('col-lg-4', false);
+    $(".content-box").children().toggleClass('col-lg-6', true);
+    $(this).hide();
+    $('#nav-close').show();
   });
 
+  $('#nav-close').click(function(e) {
+    e.preventDefault();
+    $('#sidebar').hide();
+    $("#page-content").toggleClass('col-sm-10', false);
+    $(".content-box").children().toggleClass('col-lg-4', true);
+    $(".content-box").children().toggleClass('col-lg-6', false);
+    $(this).hide();
+    $('#nav-open').show();
+  });
 
+  $('.sidebar-nav > a.active').next().show();
 
+  $('.sidebar-nav > a').click(function(e) {
+    e.preventDefault();
+    $(this).next().toggle();
+  });
 
+  $('body').scrollspy({ target: '#sidebar' });
 
+  $('#referenceversions').on('activate.bs.scrollspy', function () {
+    console.error("scroll");
+    $(this).addClass('active');
+  });
 
 });
